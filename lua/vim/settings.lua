e6a30d9a106fbe082d1891bfbe54780acf9f8f82
pGlase <pGlase>
2024-04-29 01:09:08 +0200

    Added new settings

--- lua/vim/settings.lua
+++ lua/vim/settings.lua
@@ -1,1 +1,19 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+
+vim.opt.updatetime = 50
+
 vim.cmd.colorscheme("nightfox")
